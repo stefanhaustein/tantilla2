6c4666d6719a46013945b2b93615a2d3ea9e5f9b
Stefan Haustein <stefanhaustein@Mini-von-Stefan.home>
2022-04-21 23:44:31 +0200

    Adapter

--- core/src/commonMain/kotlin/org/kobjects/tantilla2/classifier/Adapter.kt
+++ core/src/commonMain/kotlin/org/kobjects/tantilla2/classifier/Adapter.kt
@@ -3,5 +3,7 @@ package org.kobjects.tantilla2.classifier
 import org.kobjects.tantilla2.core.RuntimeContext
 import org.kobjects.tantilla2.function.Callable
 
-class Adapter(val vmt: List<Callable>, val instance: RuntimeContext) {
-}
+class Adapter(
+    val vmt: List<Callable>,
+    val instance: RuntimeContext,
+)
