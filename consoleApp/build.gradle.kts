f63e3bea619bb3b16bd59d58462430deafb0ff1b
Stefan Haustein <haustein@MacStudonStefan.home>
2022-08-29 23:51:50 +0200

    update version numbers

--- consoleApp/build.gradle.kts
+++ consoleApp/build.gradle.kts
@@ -17,9 +17,9 @@ repositories {
 
 dependencies {
     implementation(project(":core"))
-    implementation("org.kobjects.konsole:core:0.1.1")
-    implementation("org.kobjects.parserlib:core:0.2.3")
-    implementation("org.kobjects.greenspun:core:0.2.1")
+    implementation("org.kobjects.konsole:core:0.2.1")
+    implementation("org.kobjects.parserlib:core:0.3.0")
+    implementation("org.kobjects.greenspun:core:0.3.3")
 }
 
 tasks.named<JavaExec>("run") {
